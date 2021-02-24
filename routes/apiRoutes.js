83163fc7781864696ee0c60e0dd5ae6b2e2823fd
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-02-24 03:47:45 -0500

    final touches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,26 +2,43 @@ var noteData = require("../db/db.json");
 var router = require("express").Router();
 var fs = require('fs');
 
-  router.get("/notes", function(req, res) {
-    res.json(noteData);
-  });
+router.get("/notes", function (req, res) {
+  res.json(noteData);
+});
 
-  router.post("/notes", function(req, res) {
-    var notePost = req.body;
-    var id = noteData.length;
-    let post = { 
-      id : id,
-      title: notePost.title,
-      text: notePost.text 
+router.post("/notes", function (req, res) {
+  var notePost = req.body;
+  var id = noteData.length;
+  let post = {
+    id: id,
+    title: notePost.title,
+    text: notePost.text
   };
   noteData.push(post);
   let data = JSON.stringify(noteData);
   fs.writeFileSync('./db/db.json', data);
-  });
+  res.json(noteData);
+});
 
-  // router.clear("/notes/:id", function(req, res) {
-  //   res.json(noteData);
-  // });
+router.delete("/notes/:id", function (req, res) {
+  id = req.params.id
+  if (noteData.length > 1) {
+    if (id == 0) {
+      noteData.shift()
+    }
+    else {
+      noteData.splice(id, 1);
+    }
+    for (let i = id; i < noteData.length; i++) {
+      noteData[i].id = i;
+    }
+  }
+  else {
+    noteData.length = 0
+  }
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
+  res.json(noteData);
+});
 
 
 
