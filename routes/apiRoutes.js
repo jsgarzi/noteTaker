d6b37422173c23b0c6cdf0207973b5c4c2e07f7d
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-02-24 02:40:00 -0500

    post works (mostly)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,29 @@
 var noteData = require("../db/db.json");
 var router = require("express").Router();
+var fs = require('fs');
 
   router.get("/notes", function(req, res) {
-    res.json(tableData);
+    res.json(noteData);
   });
 
   router.post("/notes", function(req, res) {
-    res.json(tableData);
+    var notePost = req.body;
+    var id = noteData.length;
+    let post = { 
+      id : id,
+      title: notePost.title,
+      text: notePost.text 
+  };
+  noteData.push(post);
+  let data = JSON.stringify(noteData);
+  fs.writeFileSync('./db/db.json', data);
   });
 
-  router.clear("/notes/:id", function(req, res) {
-    res.json(tableData);
-  });
+  // router.clear("/notes/:id", function(req, res) {
+  //   res.json(noteData);
+  // });
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", htmlR);
 app.use("/api",apiR);
+app.use("/", htmlR);
 
 
 app.listen(PORT, function() {
