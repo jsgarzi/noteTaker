34a6c91684cb7ec22fbd474d2d37f286a10a4659
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-02-24 01:46:49 -0500

    html routes r good

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+var apiR = require("./routes/apiRoutes");
+var htmlR = require("./routes/htmlRoutes");
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", htmlR);
+// app.use("/api",apiR);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
